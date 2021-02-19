807b89e716d8713db6bb9f9ae9824ffc3a12f4b8
owllex <athrin42@gmail.com>
2021-02-19 18:23:04 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 console.log("loaded")
+
+function newUser() {
+  return {
+    version: "0.0.1",
+    currentClass: "Commoner",
+    classes: {
+      "Commoner": {
+        level: 1,
+        xp: 0,
+      }
+    },
+    time: {
+      lastUpdate: Date.now(),
+    }
+  }
+}
+
+var user = newUser();
+
+function progress() {
+}
