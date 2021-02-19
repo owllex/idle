78f78d7e65b7315b0e40eae228cad13ab96db98f
owllex <athrin42@gmail.com>
2021-02-19 18:54:45 -0500

    Create contentloader.js

---
+++ contentloader.js
@@ -0,0 +1,18 @@
+function loadScript() {
+  var head = document.getElementsByTagName('head')[0];
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = url;
+  head.appendChild(script);
+}
+
+const deps = [
+  "gamestate.js",
+  "messages.js",
+  "ui.js",
+  "main.js",
+]
+
+for (dep in deps) {
+  loadScript(dep);
+}
