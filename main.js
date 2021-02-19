8039165a15bd70d5dcb4284624eed7a5f4102b14
owllex <athrin42@gmail.com>
2021-02-19 18:32:50 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 console.log("loaded")
 
+function event(e, func) {
+  window.addEventListener(e, func)
+}
+
 function newUser() {
   return {
     version: "0.0.1",
@@ -20,3 +24,13 @@ var user = newUser();
 
 function progress() {
 }
+
+document.addEventListener("unload", () => {
+  saveGame()
+})
+
+event("load", () => {
+  setTimeout( () => {
+    loadGame()
+  }, 1)
+})
