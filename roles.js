adfa565f64af9669676aecc1d3a1c3c70b8b3ea5
owllex <athrin42@gmail.com>
2021-02-20 14:59:09 -0500

    Update roles.js

--- roles.js
+++ roles.js
@@ -4,4 +4,12 @@ function xpForRoleLevel(level) {
 
 function grantXp(xp) {
   user.roles[user.currentRole].xp += xp
+  let currentXp = user.roles[user.currentRole].xp
+  let xpForLevel = xpForRoleLevel(user.roles[user.currentRole].level + 1)
+  if (currentXp >= xpForLevel) {
+    // Level up
+    user.roles[user.currentRole].level += 1
+    user.roles[user.currentRole].xp -= xpForLevel
+    successMessage("Level up")
+  }
 }
