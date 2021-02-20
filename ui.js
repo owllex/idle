12074f63c16658c3049aa3892706588f6a638503
owllex <athrin42@gmail.com>
2021-02-20 12:30:38 -0500

    Update ui.js

--- ui.js
+++ ui.js
@@ -35,14 +35,23 @@ function replaceClass(cl1, cl2, id) {
   document.getElementById(id).classList.add(cl2)
 }
 
-function refreshStats() {
-  for (stat in allStats) {
-    let className = allStats[stat] + "-stat-value"
-    let elements = document.getElementsByClassName(className)
-    for (element in elements) {
-      elements[element].innerText = user.stats.current[stat]
-    }
+function fillUiElements(className, value) {
+  let elements = document.getElementsByClassName(className)
+  for (element in elements) {
+    elements[element].innerText = value
   }
+}
+
+const UI_REFRESH_STATS = allStats
+
+function refreshStats() {
+  for (stat in UI_REFRESH_STATS) {
+    let className = UI_REFRESH_STATS[stat] + "-value"
+    fillUiElements(className, user.stats.current[stat])
+  }
+  fillUiElements("role-value", user.currentRole)
+  fillUiElements("level-value, user.roles[user.currentRole].level)
+  fillUiElements("xp-value, user.roles[user.currentRole].xp)
 }
 
 function refreshUi() {
