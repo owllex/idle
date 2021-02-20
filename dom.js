11bea00d2c4fd7fe02f8f9ea609ff3870d1c57e8
owllex <athrin42@gmail.com>
2021-02-20 12:58:04 -0500

    Create dom.js

---
+++ dom.js
@@ -0,0 +1,43 @@
+function event(e, func) {window.addEventListener(e, func)}
+
+function showId(x) {
+  document.getElementById(x).style.display = ""
+}
+
+function showClass(x) {
+  let elements = document.getElementsByClassName(x)
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.display = ""
+  }
+}
+
+function hideId(x) {
+  document.getElementById(x).style.display = "none"
+}
+
+function hideClass(x) {
+  let elements = document.getElementsByClassName(x)
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.display = "none"
+  }
+}
+
+function addClass(cl, id) {
+  document.getElementById(id).classList.add(cl)
+}
+
+function removeClass(cl, id) {
+  document.getElementById(id).classList.remove(cl)
+}
+
+function replaceClass(cl1, cl2, id) {
+  document.getElementById(id).classList.remove(cl1)
+  document.getElementById(id).classList.add(cl2)
+}
+
+function fillUiElements(className, value) {
+  let elements = document.getElementsByClassName(className)
+  for (element in elements) {
+    elements[element].innerText = value
+  }
+}
