387096d8f3cbf1102dd139ea51b48576a614ff39
owllex <athrin42@gmail.com>
2021-03-04 20:38:02 -0500

    Update formatting.js

--- formatting.js
+++ formatting.js
@@ -7,27 +7,53 @@ const FULL_BLOCK_CHAR = '█'
 const HALF_BLOCK_CHAR = '▄'
 const EMPTY_BLOCK_CHAR = ' '
 
-function formatProgressBar(value, max, length, label, includeValues, showAsPercent) {
-  const fractionalBlocks = value / max * (length - 2)
-  const blocks = Math.floor(fractionalBlocks)
-  const empties = (length - 2) - blocks
-  const frac = fractionalBlocks - blocks
-  let midBlock = EMPTY_BLOCK_CHAR
-  if (frac >= 0.5) {
-    midBlock = HALF_BLOCK_CHAR
+class ProgressBar {
+  constructor(value, max) {
+    this.value = value
+    this.max = max
+    this.length = DEFAULT_PROGRESS_BAR_LENGTH
+    this.label = ''
   }
-  let result = label + " " + LEFT_BAR_CHAR + FULL_BLOCK_CHAR.repeat(blocks) +
-      midBlock + EMPTY_BLOCK_CHAR.repeat(empties) + RIGHT_BAR_CHAR
-  if (includeValues) {
-    if (showAsPercent) {
-      result += " " + Math.floor((value / max * 100)) + "%"
-    } else {
-      result += " " + formatNumber(value) + " / " + formatNumber(max)
+  
+  setLength(length) {
+    this.length = length
+    return this
+  }
+  
+  includeValue() {
+    this.includeValue = true
+    return this
+  }
+  
+  showAsPercent() {
+    this.showAsPercent = true
+    return this
+  }
+  
+  setLabel(label) {
+    this.label = label
+    return this
+  }
+  
+  format() {
+    const fractionalBlocks = this.value / this.max * (this.length - 2)
+    const blocks = Math.floor(fractionalBlocks)
+    const empties = (this.length - 2) - blocks
+    const frac = fractionalBlocks - blocks
+    let midBlock = EMPTY_BLOCK_CHAR
+    if (frac >= 0.5) {
+      midBlock = HALF_BLOCK_CHAR
     }
+    let result = this.label + " " + LEFT_BAR_CHAR + FULL_BLOCK_CHAR.repeat(blocks) +
+        midBlock + EMPTY_BLOCK_CHAR.repeat(empties) + RIGHT_BAR_CHAR
+    if (this.includeValues) {
+      if (this.showAsPercent) {
+        result += " " + Math.floor(this.value / this.max * 100) + "%"
+      } else {
+        result += " " + formatNumber(this.value) + " / " + formatNumber(this.max)
+      }
+    }
+    return result
   }
-  return result
 }
 
-function formatDefaultProgressBar(value, max, label, includeValue, showAsPercent) {
-  return formatDefaultProgressBar(value, max, DEFAULT_PROGRESS_BAR_LENGTH, label, includeValue, showAsPercent)
-}
