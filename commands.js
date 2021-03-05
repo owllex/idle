50ea5ec20a4f34055f9c73657dd2e2d6abbb7d42
owllex <athrin42@gmail.com>
2021-03-04 20:24:15 -0500

    Update commands.js

--- commands.js
+++ commands.js
@@ -17,8 +17,11 @@ function logInput(text, output) {
 }
 
 // ▉▉▙
-const BLOCK_CHAR = '█'
-const HALF_BLOCK_CHAR = '▌'
+const LEFT_BAR_CHAR = '['
+const RIGHT_BAR_CHAR = ']'
+const FULL_BLOCK_CHAR = '█'
+const HALF_BLOCK_CHAR = '▄'
+const EMPTY_BLOCK_CHAR = '_'
 
 function formatProgressBar(value, max, length, label, includeValues, showAsPercent) {
   const fractionalBlocks = value / max * (length - 2)
@@ -29,7 +32,8 @@ function formatProgressBar(value, max, length, label, includeValues, showAsPerce
   if (frac >= 0.5) {
     midBlock = HALF_BLOCK_CHAR
   }
-  let result = label + " [" + BLOCK_CHAR.repeat(blocks) + midBlock + " ".repeat(empties) + "]"
+  let result = label + " " + LEFT_BAR_CHAR + FULL_BLOCK_CHAR.repeat(blocks) +
+      midBlock + EMPTY_BLOCK_CHAR.repeat(empties) + RIGHT_BAR_CHAR
   if (includeValues) {
     if (showAsPercent) {
       result += " " + Math.floor((value / max * 100)) + "%"
