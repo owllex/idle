cc0cfb61ef6a37c45ab6afa95489615f40ffadb0
owllex <athrin42@gmail.com>
2021-03-05 23:27:22 -0500

    Update roles.js

--- roles.js
+++ roles.js
@@ -1,3 +1,11 @@
+const allRoles = [
+  "Commoner",
+  "Fighter",
+  "Rogue",
+  "Apprentice",
+  "Acolyte",
+]
+
 function xpForRoleLevel(level) {
   return 68 + Math.pow(level, 5)
 }
@@ -16,5 +24,23 @@ function grantXp(xp) {
     } else {
       break;
     }
+  }
+}
+
+function getBonusBlockForRole(role, level) {
+  switch (role) {
+    case "Commoner":
+      return {active: {maxHp: level - 1}, global: {maxHp: level - 1}}
+    case "Fighter":
+      return {active: {str: level - 1}, global: {str: level - 1}}
+    case "Apprentice":
+      return {active: {int: level - 1}, global: {int: level - 1}}
+    case "Rogue":
+      return {active: {agi: level - 1}, global: {agi: level - 1}}
+    case "Acolyte":
+      return {active: {wis: level - 1}, global: {wis: level - 1}}
+    default:
+      console.log("No role " + role);
+      return {active: {}, global: {}}
   }
 }
