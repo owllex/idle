75e99fa9934aae145842f3c99dd8d8fad09fd430
owllex <athrin42@gmail.com>
2021-03-05 23:28:40 -0500

    Update roles.js

--- roles.js
+++ roles.js
@@ -10,23 +10,6 @@ function xpForRoleLevel(level) {
   return 68 + Math.pow(level, 5)
 }
 
-function grantXp(xp) {
-  user.roles[user.currentRole].xp += xp
-  while (true) {
-    let currentXp = user.roles[user.currentRole].xp
-    let xpForLevel = xpForRoleLevel(user.roles[user.currentRole].level + 1)
-    if (currentXp >= xpForLevel) {
-      // Level up
-      user.roles[user.currentRole].level += 1
-      user.roles[user.currentRole].xp -= xpForLevel
-      updateStats()
-      successMessage("Level up")
-    } else {
-      break;
-    }
-  }
-}
-
 function getBonusBlockForRole(role, level) {
   switch (role) {
     case "Commoner":
