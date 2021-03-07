9faa168148fa2e1b2b94f905dc1abdfa999880a9
owllex <athrin42@gmail.com>
2021-03-07 00:35:52 -0500

    Update activity.js

--- activity.js
+++ activity.js
@@ -1,5 +1,10 @@
 const ALL_LOCATIONS = {
-  "town": {short: "Altheria", long: "The city of Altheria"},
+  "tutorial": {short: "Tutorial", long: "Tutorial"},
+  "town": {short: "Altheria", long: "The city of Altheria", locations: {
+    "market": {short: "Market", long: "The Grand Market"},
+    "sigilkeep": {short: "Sigilkeep", long: "The Spire of Sigilkeep"},
+    "port": {short: "Port", long: "The Lower City Port"},
+  }},
   "home": {short: "Home", long: "Home"},
   "mistwood": {short: "Mistwood", long: "The Mistwood"},
 }
