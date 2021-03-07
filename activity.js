1519b2b0f524f69e3ba943838efa0fde73e2d11d
owllex <athrin42@gmail.com>
2021-03-07 00:32:06 -0500

    Create activity.js

---
+++ activity.js
@@ -0,0 +1,5 @@
+const ALL_LOCATIONS = {
+  "town": {short: "Altheria", long: "The city of Altheria"},
+  "home": {short: "Home", long: "Home"},
+  "mistwood": {short: "Mistwood", long: "The Mistwood"},
+}
